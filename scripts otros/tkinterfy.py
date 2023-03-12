accdd88488078991ff9f575a5bf0fc5357cdd854
sebacris97 <51713904+sebacris97@users.noreply.github.com>
2023-03-12 15:22:25 -0300

    Add files via upload

--- scripts otros/tkinterfy.py
+++ scripts otros/tkinterfy.py
@@ -2,86 +2,140 @@ import spotipy
 import spotipy.util as util
 from tkinter import *
 from tkinter.scrolledtext import *
-from az import get_lyrics
+from unidecode import unidecode
+#from az import get_lyrics
+#import azapi
+from lyrics_extractor import SongLyrics
 
-def login_spotify(username,client_id,client_secret,redirect_url,scope):
-    # Set up credentials and authenticate with Spotify API
-    token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_url)
-    return spotipy.Spotify(auth=token)
+class LyricsWindow(Tk):
 
-def create_window(title,size,topmost):
-    # Create a window to display the information
-    root = Tk()
-    root.geometry("310x410")
-    root.title("Current Track Info")
-    root.attributes('-topmost', 'true') #que este siempre encima de todo
-    return root
+    def __init__(self):
+        super().__init__()
 
-def setting_up_window(root):
-    title = StringVar()
-    artist = StringVar()
-    album = StringVar()
-    contador = DoubleVar()
-    title_label = Label(root,textvariable = title)
-    artist_label = Label(root,textvariable = artist)
-    album_label = Label(root,textvariable = album)
-    contador_label = Label(root,textvariable = contador)
-    text=ScrolledText(root, font="Verdana 10",wrap=WORD)
-    text.pack() #con pack la pongo en la ventana
-    return [title,contador,text,artist,album]
+        #self.API = azapi.AZlyrics()
+        GCS_API_KEY = 'AIzaSyCfPLivvWZu_rLf3dCceZMWxvNS6NQ2SVk'
+        GCS_ENGINE_ID = 'b68b26a7f065541a7'
+        self.API = SongLyrics(GCS_API_KEY, GCS_ENGINE_ID)
 
+        username = 'sebacris97'
+        client_id = '4809312ab757475e83bdf86881c70558'
+        client_secret = '883ba1bdd5fd4b63829a826160d05b90'
+        redirect_url = 'http://localhost:8888/callback'
+        scope = 'user-read-currently-playing'
+        self.sp = self.login_spotify(username,client_id,client_secret,redirect_url,scope)
 
-def actualizar():
-    current_track = sp.current_user_playing_track()
-    # Extract relevant information from response
-    if current_track is not None:
+        size = "345x410"
+        window_title = "Current track info"
+        topmost = True #que este siempre encima de todo  
+        self.configure_window(window_title,size,topmost)
+
+        #recibe una ventana de parametro
+        self.song_title,self.album,self.artist, self.song_time = self.setting_up_window()
+
+        self.actualizar()
+    
+    def login_spotify(self,username,client_id,client_secret,redirect_url,scope):
+        # Set up credentials and authenticate with Spotify API
+        token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_url)
+        return spotipy.Spotify(auth=token)
+
+    def configure_window(self,title,size,topmost):
+        # Create a window to display the information
+        self.geometry(size)
+        self.title("Current Track Info")
+        self.attributes('-topmost', 'true') #que este siempre encima de todo
+
+    def setting_up_window(self):
+        title = StringVar()
+        album = StringVar()
+        artist = StringVar()
+        song_time = StringVar()
+        title_label = Label(self,textvariable = title)
+        album_label = Label(self,textvariable = album)
+        artist_label = Label(self,textvariable = artist)
+        song_time_label = Label(self,textvariable = song_time)
+        self.lyrics_text = ScrolledText(self, wrap=WORD, font="Verdana 10")
+        self.lyrics_text.bind("<Key>", lambda e: "break") #desactiva el escribir
+        title_label.pack()
+        album_label.pack()
+        artist_label.pack()
+        song_time_label.pack()
+        self.lyrics_text.pack()
+        return [title,album,artist,song_time]
+
+    #imprimir minutos en formato mm:ss
+    def tiempo(self,current_time, total_time):
+        minutes1,seconds1 = divmod(current_time / 1000, 60)
+        minutes2,seconds2 = divmod(total_time / 1000, 60)
+        current_time = f"{minutes1:02.0f}:{seconds1:05.2f}".split('.')[0]
+        total_time = f"{minutes2:02.0f}:{seconds2:05.2f}".split('.')[0]
+        return current_time+' - '+total_time
+
+    def check_song(self):
+        current_track = self.sp.current_user_playing_track()
+        if current_track is not None and current_track['currently_playing_type'] != 'episode':
+            try:
+                self.track_name = current_track['item']['name']
+                self.track_artist = current_track['item']['artists'][0]['name']
+                self.track_album = current_track['item']['album']['name']
+                self.current_time = current_track["progress_ms"]
+                self.total_time = current_track['item']["duration_ms"]
+                return current_track
+            except:
+                self.check_song()
+        else:
+            return None
+            
+    def actualizar_lyrics_text(self):
+        #self.API.artist = unidecode(self.track_artist) #cambio caracteres de otro idioma por ingles
+        #self.API.title = unidecode(self.track_name)
+        #lyrics = self.API.getLyrics(ext='lrc')
+        #self.lyrics_text.insert(END, lyrics if type(lyrics) != int else 'No lyrics found')
         try:
-            track_name = current_track['item']['name']
-            artist_name = current_track['item']['artists'][0]['name']
-            album_name = current_track['item']['album']['name']
+            lyrics = self.API.get_lyrics(unidecode(self.track_name+' '+self.track_artist))['lyrics']
+            self.lyrics_text.insert(END, lyrics)
+            self.lyrics_text.insert(1.0,'\n\n\n')
+            self.lyrics_text.insert(END,'\n\n\n')
         except:
-            track_name = ''
-            artist_name = ''
-            album_name = ''
-            pass
-        try:
-            text.yview(contador.get())
-            contador.set(contador.get()+0.25) #0.25 es la velocidad a la que scrollea las letras
-        except:
-            pass
-        if title.get() != track_name:
-            contador.set(0) #empieza cancion nueva el contador vuelve a 0
-            text.delete('1.0',END) #se borran las letras de la cancion anterior     
-            title.set(track_name)
-            artist.set(artist_name)
-            album.set(album_name)
-            title.set(track_name) #le ponemos a la variable title el titulo de la cancion actual
-            text.insert(END, track_name+'\n')
-            text.insert(END, artist_name+'\n')
-            text.insert(END, album_name+'\n\n')
-            #getting the lyrics
-            text.insert(END, get_lyrics(artist_name, track_name))
-    else:
-        text.delete('1.0',END)
-        text.insert(END, 'no song playing')
-        title.set('')
-    #llamado recursivo cada 500ms
-    root.after(500,actualizar)
+            self.lyrics_text.insert(END, 'No lyrics found')
 
-username = 'sebacris97'
-client_id = '4809312ab757475e83bdf86881c70558'
-client_secret = '883ba1bdd5fd4b63829a826160d05b90'
-redirect_url = 'http://localhost:8888/callback'
-scope = 'user-read-currently-playing'
-sp = login_spotify(username,client_id,client_secret,redirect_url,scope)
+    def actualizar_tiempo(self):
+        self.song_time.set(self.tiempo(self.current_time,self.total_time))
 
-size = "310x410"
-window_title = "Current track info"
-topmost = True #que este siempre encima de todo  
-root = create_window(window_title,size,topmost)
-
-#recibe una ventana de parametro
-title,contador,text,artist,album = setting_up_window(root)
+    def actualizar_y(self):
+        cociente = (self.current_time/self.total_time)*0.725
+        self.lyrics_text.yview_moveto(cociente)
         
-root.after(500,actualizar)
+    def actualizar(self):
 
+        if self.check_song() != None:
+
+            self.actualizar_y()
+            self.actualizar_tiempo()
+
+            if self.song_title.get() != self.track_name: #si cambio la cancion
+
+                self.lyrics_text.delete('1.0',END)
+                self.song_title.set(self.track_name)
+                self.album.set(self.track_album)
+                self.artist.set(self.track_artist)
+                self.actualizar_lyrics_text()
+        else:
+            self.song_title.set('No song\'s playing')
+            self.album.set('')
+            self.artist.set('')
+            self.song_time.set('')
+            self.lyrics_text.delete('1.0',END)
+            self.lyrics_text.insert(END, 'no lyrics to show')
+            self.title("Current Track Info")
+            
+        self.after(250,self.actualizar) #llamado recursivo cada 500ms
+        
+
+def main():
+    window = LyricsWindow()
+    window.mainloop()
+    
+
+if __name__ == "__main__":
+    main()
